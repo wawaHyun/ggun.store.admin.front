b257620871748936b78eaee9c9e1bb4c5ee798c7
wawaHyun <sjrkchdkdy@gmail.com>
2024-06-13 12:56:00 +0900

    240613

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,30 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// module.exports = {
+//     reactStrictMode: true,
+//     async rewrites() {
+//       return [
+//         {
+//           source: "/humans.txt",
+//           destination: "/api/humans",
+//         },
+//       ];
+//     },
+//     publicRuntimeConfig: {
+//       modifiedDate: new Date().toISOString(),
+//     },
+//   };
+
+// const fs = require("fs");
+
+// const version = fs.readFileSync("./.version", "utf8");
+// const lastUpdatedAt = fs.readFileSync("./.last-updated", "utf8");
+
+// module.exports = {
+//   env: {
+//     NEXT_PUBLIC_PACKAGE_VERSION: version,
+//     NEXT_PUBLIC_LAST_UPDATED_AT: lastUpdatedAt,
+//   },
+// };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
+  
+  const update = () => {
+    const today = new Date();
+    const usaTime = today.getTime() + (today.getTimezoneOffset() * 60 * 1000);
+    const korTime = new Date(usaTime + (9 * 60 * 60 * 1000))
+    return korTime+"";
+  }
   return (
     <div className="w-screen h-screen">
       <div className="h-1/2 bg-pink-200">
+        update : {update()} <br /><br />
         I love pink!
       </div>
       <div className="h-1/2 bg-sky-200">
